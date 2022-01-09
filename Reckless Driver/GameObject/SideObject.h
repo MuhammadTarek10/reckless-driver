4836f28ec60a5bfeb493915a91784ee3b6c96682
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-01-09 22:18:59 +0200

    removed Implementation from header

--- Reckless Driver/GameObject/SideObject.h
+++ Reckless Driver/GameObject/SideObject.h
@@ -7,12 +7,8 @@ private:
     int m_Damage;
 public:
     SideObject(int damage, const std::string &name): m_Damage(damage), GameObject(name){};
-    void SetDamage(int damage){
-        m_Damage = damage;
-    };
-    int GetDamage(){
-        return m_Damage;
-    };
+    void SetDamage(int damage);
+    int GetDamage();
 };
 
 #endif // SIDEOBJECT_H
