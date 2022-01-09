4d32879b2214552aa02c5052887ad20c893b7ca7
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-01-09 22:28:54 +0200

    made main with some prints with car

--- Reckless Driver/main.cpp
+++ Reckless Driver/main.cpp
@@ -5,6 +5,23 @@
 #include "GameObject/FireHydrant.h"
 
 int main(void){
-    FireHydrant f(123, "S");
+    Car car("Manufacturer", 25, 1223);
+    car.Dashboard();
+    car.Accelerate();
+    car.Dashboard();
+    car.SwitchOn();
+    car.Accelerate();
+    car.Dashboard();
+    car.Accelerate();
+    car.Dashboard();
+    car.FillFuel(44);
+    car.Accelerate();
+    car.Accelerate();
+    car.Accelerate();
+    car.Accelerate();
+    car.Accelerate();
+    car.Dashboard();
+    car.Brake();
+    car.Dashboard();
     return 0;
 }
