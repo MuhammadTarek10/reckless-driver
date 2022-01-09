bc58916e39bb496c6816d8e2281c21b9fa1142c8
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-01-09 22:19:23 +0200

    Removed a space

--- Reckless Driver/GameObject/FireHydrant.cpp
+++ Reckless Driver/GameObject/FireHydrant.cpp
@@ -3,7 +3,6 @@
 
 FireHydrant::FireHydrant(int damage, const std::string &name):SideObject(damage, name){}
 
-
 void FireHydrant::onCollision(GameObject *pOther){
     std::cout << "Fire" << std::endl;
 }
