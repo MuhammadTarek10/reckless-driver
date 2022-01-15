f11eb9d7bb4f79bdd8f99b5ea56233fc8eb1a85e
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-01-15 23:44:12 +0200

    Changed name, Reckless Driver to Source

--- Reckless Driver/GameObject/FireHydrant.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "FireHydrant.h"
-
-FireHydrant::FireHydrant(int damage, const std::string &name):SideObject(damage, name){}
-
-void FireHydrant::onCollision(GameObject *pOther){
-    std::cout << "Fire" << std::endl;
-}
